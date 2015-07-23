519258fc8e0262d260d77ef080883cac78659742
xuejianqing <xuejianqing@star-net.cn>
2015-07-23 16:45:57 +0800

    Generate yaml static library

--- Android.mk
+++ Android.mk
@@ -1,12 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libyaml
-
-LOCAL_MODULE_FILENAME := libyaml
-
-LOCAL_SRC_FILES := \
+libyaml_src_files := \
     src/api.c \
     src/reader.c \
     src/scanner.c \
@@ -16,14 +10,20 @@ LOCAL_SRC_FILES := \
     src/emitter.c \
     src/dumper.c
 
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH) \
-    $(LOCAL_PATH)/include 
-
+include $(CLEAR_VARS)
+LOCAL_MODULE := libyaml
+LOCAL_SRC_FILES := $(libyaml_src_files)
+LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/include
 LOCAL_CFLAGS := -DHAVE_CONFIG_H
-
 LOCAL_C_EXPORTS := $(LOCAL_PATH)/include
-
 LOCAL_MODULE_TAGS := debug
-
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libyaml
+LOCAL_SRC_FILES := $(libyaml_src_files)
+LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/include
+LOCAL_CFLAGS += -DHAVE_CONFIG_H
+LOCAL_MODULE_TAGS := debug
+include $(BUILD_STATIC_LIBRARY)
+
