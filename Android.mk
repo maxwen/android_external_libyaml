5129a08c360a884ac4d25f8d24372f6c9816ac3a
maxwen <max.weninger@gmail.com>
2022-05-13 11:34:30 +0200

    simplify Android.mk

--- Android.mk
+++ Android.mk
@@ -13,17 +13,9 @@ libyaml_src_files := \
 include $(CLEAR_VARS)
 LOCAL_MODULE := libyaml
 LOCAL_SRC_FILES := $(libyaml_src_files)
-LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/include
-LOCAL_CFLAGS := -DHAVE_CONFIG_H
-LOCAL_C_EXPORTS := $(LOCAL_PATH)/include
-LOCAL_MODULE_TAGS := debug
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_CFLAGS := -DHAVE_CONFIG_H -Wno-unused-parameter
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+LOCAL_VENDOR_MODULE := true
+LOCAL_MULTILIB := both
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libyaml
-LOCAL_SRC_FILES := $(libyaml_src_files)
-LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/include
-LOCAL_CFLAGS += -DHAVE_CONFIG_H
-LOCAL_MODULE_TAGS := debug
-include $(BUILD_STATIC_LIBRARY)
-
